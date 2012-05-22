fb94fc20be42346db528dcdb2838759896484f51
Prithvi <kv.prithvi5@gmail.com>
2012-05-22 16:36:48 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Meet4lunch::Application.config.secret_token = '5d7b800b4e586cda4ab5ecf19c33511826bb945ce9e54988b820ae3eeef635ff8ea8802b4f4d9e1e29921bf126641dc5083c2351edc8d9bd4cf6a2ee55231dfc'
