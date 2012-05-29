6131568142a778bb63b45ffdbf4582a580ac4e01
Prithvi <kv.prithvi5@gmail.com>
2012-05-29 23:13:36 +0800

    final

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+ before_filter :signed_in_user, only: [:edit, :update]
+ before_filter :correct_user,   only: [:edit, :update]
   def show
     @user = User.find(params[:id])
   end
@@ -17,4 +19,30 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+   def edit
+    #@user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+	   flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+
+    def signed_in_user
+      redirect_to signin_path, notice: "Please sign in." unless signed_in?
+    end
+  def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -52,4 +52,26 @@ describe "User pages" do
       end
     end
   end
+  
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+     before do
+      sign_in user
+      visit edit_user_path(user)
+    end
+
+    describe "page" do
+      it { should have_selector('h1',    text: "Update your profile") }
+      it { should have_selector('title', text: "Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+
+      it { should have_content('error') }
+    end
+  end
+  
+  
 end
